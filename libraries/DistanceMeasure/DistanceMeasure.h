bb242ae3212bead66657e5b723ee3dccbc4dbbd0
Keeyan Nejad <keeyan@keeyan.xyz>
2019-03-24 10:43:09 +0000

    Add distance measure library to calculate distance travelled

---
+++ libraries/DistanceMeasure/DistanceMeasure.h
@@ -0,0 +1,32 @@
+#include <InterruptBasedSpeedMeasure.h>
+
+class DistanceMeasure {
+	private:
+		InterruptSpeedMeasure * left_wheel;
+		InterruptSpeedMeasure * right_wheel;
+		float total_interrupts_on_wheel;
+		float wheel_circumference_mm;
+
+		float get_average_number_of_wheel_rotations()
+		{
+			float left_wheel_rotations = ((float)left_wheel->GetkDistanceCount())/total_interrupts_on_wheel;
+			float right_wheel_rotations = ((float)right_wheel->GetkDistanceCount())/total_interrupts_on_wheel;
+
+			return (left_wheel_rotations+right_wheel_rotations)/2.0;
+		}
+
+	public:
+		DistanceMeasure() {};
+		DistanceMeasure(float wheel_diameter_mm, InterruptSpeedMeasure * left_wheel, InterruptSpeedMeasure * right_wheel)
+			: left_wheel(left_wheel), right_wheel(right_wheel)
+		{
+			total_interrupts_on_wheel = right_wheel->get_tot_interr_on_circle();
+			wheel_circumference_mm = wheel_diameter_mm * 3.1416;
+		}
+
+		float get_distance_travelled_mm()
+		{
+			float number_of_wheel_rotations = get_average_number_of_wheel_rotations();
+			return number_of_wheel_rotations * wheel_circumference_mm;
+		}
+};
--- libraries/Double_driver/double_driver.h
+++ libraries/Double_driver/double_driver.h
@@ -9,9 +9,10 @@
 #include<InterruptBasedSpeedMeasure.h>
 #include<InterruptBasedInputs.h>
 #include<basic_speed_PID.h>
+#include<DistanceMeasure.h>
 
 
-bool verbose = true; //TURN ON FOR DEBUGGING
+bool verbose = false; //TURN ON FOR DEBUGGING
 bool printed = false;
 enum command_list {start_R, start_L, stop_R, stop_L, reverse_R, reverse_L};
 
@@ -21,40 +22,43 @@ double pid_out_R;
 double pid_out_L;
 
 class double_driver{
-	
+
 	protected:
 
 		// Objects
 		basic_speed_PID pid_R, pid_L;
 		HBridgeDCmotor motor_R, motor_L;
 		InterruptSpeedMeasure rotation_counter_R, rotation_counter_L;
-		IntervalCheckTimer speed_check;		
-		
-	public:	
-	
+		IntervalCheckTimer speed_check;
+		DistanceMeasure distance_measure;
+
+	public:
+
 		// Constructor
-		double_driver(){		}
-		
+		double_driver(){}
+
 		// Set up motor
 		void setup_motor(int motorpin_R, int directionpin_R, int motorpin_L, int directionpin_L)
 		{
 			if(verbose)
 				Serial.println("Motor setup");
-				
+
 			motor_R.setup_HBridgeDCmotor(motorpin_R,directionpin_R);
-  			motor_L.setup_HBridgeDCmotor(motorpin_L,directionpin_L);
+			motor_L.setup_HBridgeDCmotor(motorpin_L,directionpin_L);
 		}
-		
+
 		// Set up speed measuring unit, hall effect sensor
 		void setup_speed_measure(ArduinoInterruptNames measure_pin_R, ArduinoInterruptNames measure_pin_L)  // int_0 = pin 2, int_1 = pin 3
 		{
 			if(verbose)
 				Serial.println("Speed measure setup");
-			
+
 			rotation_counter_R.setupSpeedMeasure(measure_pin_R);
 			rotation_counter_L.setupSpeedMeasure(measure_pin_L);
+
+			distance_measure = DistanceMeasure(57, &rotation_counter_L, &rotation_counter_R);
 		}
-		
+
 		//Set up PID values
 		void setup_pid_R(){
 			double kp=1; 
@@ -63,9 +67,9 @@ class double_driver{
 			double PIDoutMin=0;
 			double PIDoutMax=255;
 			pid_R= basic_speed_PID(kp, ki, kd, PIDoutMin, PIDoutMax);
-			
+
 		}
-		
+
 		void setup_pid_L(){
 			double kp=1; 
 			double ki=0.1; 
@@ -73,9 +77,9 @@ class double_driver{
 			double PIDoutMin=0;
 			double PIDoutMax=255;
 			pid_L = basic_speed_PID(kp, ki, kd, PIDoutMin, PIDoutMax);
-			
+
 		}
-		
+
 		// Set times between checks for components and system
 		void set_time_intervals(int target_speed_time)
 		{
@@ -83,15 +87,15 @@ class double_driver{
 				Serial.println("Time interval setup");
 			speed_check.setInterCheck(target_speed_time);
 		}
-		
+
 		void set_target_speed_R(int speed_R){
 			target_speed_R = speed_R;
 		}
-		        
+
 		void set_target_speed_L(int speed_L){
 			target_speed_L = speed_L;
 		}
-		
+
 		// Determine motor speed command (low, mid, high) from pushbuttons
 		void motor_speed_input(command_list command)
 		{	
@@ -100,94 +104,94 @@ class double_driver{
 				case start_R:
 					if(verbose)
 						Serial.println("	Started RIGHT");
-					
+
 					motor_R.start();	
-        			break;
-        		
-        		case start_L:
+					break;
+
+				case start_L:
 					if(verbose)
 						Serial.println("	Started LEFT");
-					
+
 					motor_L.start();	
-        			break;
-        		
-        		case stop_R:
-	        		if(verbose)
+					break;
+
+				case stop_R:
+					if(verbose)
 						Serial.println("	Stopped RIGHT");  
-	        		
+
 					motor_R.stop();
-	        		target_speed_R=0; 
-	        		break;
-	        		
-        		case stop_L:
-	        		if(verbose)
+					target_speed_R=0; 
+					break;
+
+				case stop_L:
+					if(verbose)
 						Serial.println("	Stopped LEFT");  
-	        		
+
 					motor_L.stop();
-	        		target_speed_L=0; 
-	        		break;
-        			
+					target_speed_L=0; 
+					break;
+
 				case reverse_R:
-	        		if(verbose)
+					if(verbose)
 						Serial.println("	Reversing RIGHT");
-	        		motor_R.changedir();
-					
-	        		break;
-        
-        		case reverse_L:
-	        		if(verbose)
+					motor_R.changedir();
+
+					break;
+
+				case reverse_L:
+					if(verbose)
 						Serial.println("	Reversing LEFT");
 					motor_L.changedir();
-	        		break;
-	        
-        		default:
-          			break;
+					break;
+
+				default:
+					break;
 			}
-			
+
 		}
-		
+
 		// Get motor speed from Hall effect sensor (RPM)
 		double read_motor_speed(int side)
 		{
 			double RPM;
 			if(side == 0){
 				RPM=rotation_counter_R.getRPMandUpdate();
-//    			Serial.print("Right: ");
-//				Serial.print(RPM);
+				//    			Serial.print("Right: ");
+				//				Serial.print(RPM);
 			}
 			if(side == 1){
 				RPM=rotation_counter_L.getRPMandUpdate();
-//    			Serial.print(" Left: ");
-//    			Serial.println(RPM);
-    		}
+				//    			Serial.print(" Left: ");
+				//    			Serial.println(RPM);
+			}
 			return RPM;
-    		
+
 		}
-		
+
 		// Execute the system task
 		void execute_task1()
 		{		
 			if(verbose && printed == false){
-			printed = true;
-			Serial.println("Running task_1");
+				printed = true;
+				Serial.println("Running task_1");
 			}
-			
+
 			double curr_speed_R;
 			double curr_speed_L;
-			
-			
+
+
 			if(!motor_R.isStarted())
 				motor_speed_input(start_R);
 			if(!motor_L.isStarted())
 				motor_speed_input(start_L);
-							
+
 			// PID controller to adjust speed to set point, use target speed check
 			if(speed_check.isMinChekTimeElapsedAndUpdate()){
 				curr_speed_R = read_motor_speed(0);
 				curr_speed_L = read_motor_speed(1);
-				
-			
-			
+
+
+
 				if(motor_R.isStarted())
 				{
 					Serial.print("RIGHT: ");
@@ -199,9 +203,9 @@ class double_driver{
 					Serial.print(" ");
 					pid_out_R = pid_R.ComputePID_output(target_speed_R, curr_speed_R);
 					motor_R.setSpeedPWM(pid_out_R);
-				
-					
-					
+
+
+
 				}
 				if(motor_L.isStarted())
 				{
@@ -209,24 +213,29 @@ class double_driver{
 					Serial.print(curr_speed_L);
 					Serial.print(" ");
 					Serial.print(target_speed_L);
-					
+
 					Serial.print(" PWM left: ");
 					Serial.print(pid_out_L);
 					Serial.print(" ");
 					pid_out_L = pid_L.ComputePID_output(target_speed_L, curr_speed_L);
 					motor_L.setSpeedPWM(pid_out_L);
-					
-										
+
+
 
 					Serial.println("-------------------");
 				}
-//				else
-//					pid_L.reset_pidcontrol();
-//					pid_R.reset_pidcontrol();
+
+				Serial.println(rotation_counter_R.GetkDistanceCount());
+				Serial.println(rotation_counter_L.GetkDistanceCount());
+
+				//				else
+				//					pid_L.reset_pidcontrol();
+				//					pid_R.reset_pidcontrol();
 			}
-			
+
+			Serial.println(distance_measure.get_distance_travelled_mm());
+
 		}
 };
-
 
 #endif
